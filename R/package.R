6557e58b76f8996b3f5f72b20b467d6b362b1002
Sang T. Truong <sttruong@stanford.edu>
2021-06-21 00:51:28 -0400

    import wl_convert_units

--- R/package.R
+++ R/package.R
@@ -10,5 +10,6 @@
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
+#' @importFrom hyperSpec wl_convert_units
 ## usethis namespace: end
 NULL
